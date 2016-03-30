6752b5e6b2e22c80d956f13e36ad2cfc386b37a6
AdrianArmaselu <adi.armaselu@gmail.com>
2016-03-30 09:06:31 -0500

    Added files via upload
    Updated the NGram and Generation Model

--- NGram.py
+++ NGram.py
@@ -1,161 +1,199 @@
 from collections import OrderedDict
+from random import randint
 import midi
 from midi.events import NoteOnEvent
 from midi.events import NoteOffEvent
+from graphmodel.MidiIO import MidiIO
+from graphmodel.Objects import SoundEvent, is_chord
 
 __author__ = 'Adisor'
 
 
 # TODO: HANDLE CHANNELS IN A NEW WAY
-# TODO: OFFSET PITCHES IF IT INCREASES DISTRIBUTION COUNTS
+# TODO: HANDLE ALL CHANNELS
 # TODO: RESOLUTION METHOD FOR SONGS METADATAS
+# TODO: CONSIDER IGNORING CHANNEL
+# TODO: INCLUDE CHANNEL IN NGram model
+#
+"""
+TODO: Methods for increasing distribution counts:
+    velocity tolerance
+    ticks tolerance
+    duration tolerance
+    pitch offset from other song
+    pitch tolerance
+"""
 
 
-class Note(object):
-    def __init__(self, start_tick, ticks, channel, pitch, velocity):
-        self.start_tick = start_tick
-        self.ticks = ticks
-        self.channel = channel
-        self.pitch = pitch
-        self.velocity = velocity
-
-    # ticks | channel | pitch | velocity
-    # bytes: 12 | 5 | 7 | 8
-    def encode(self):
-        return (self.ticks << 20) | (self.channel << 15) | (self.pitch << 8) | self.velocity
-
-    def __hash__(self):
-        return self.encode()
-
-    def __str__(self):
-        return ("Note(start_tick:%d, ticks:%d, channel:%d, pitch:%d, velocity:%d)" %
-                (self.start_tick, self.ticks, self.channel, self.pitch, self.velocity))
+def print_tuple(t):
+    for event in t:
+        print event, "----"
 
 
-def get_note_mappings(midi_file):
-    mappings = {}
-    running_events = {}
-    cumulative_tick = 0
-    pattern = midi.read_midifile(midi_file)
-    for track in pattern:
-        for event in track:
-            cumulative_tick += event.tick
-            event_type = type(event)
-            if event_type != NoteOnEvent and event_type != NoteOffEvent:
-                continue
-            if event_type == NoteOffEvent or (event_type == NoteOnEvent and event.velocity == 0):
-                (event_tick, event) = running_events[event.pitch][event.channel]
-                del running_events[event.pitch][event.channel]
-                ticks = cumulative_tick - event_tick
-                new_note = Note(event_tick, ticks, event.channel, event.pitch, event.velocity)
-                start_tick = new_note.start_tick
-                channel = new_note.channel
-                if start_tick not in mappings:
-                    mappings[start_tick] = {}
-                if new_note.channel not in mappings[start_tick]:
-                    mappings[start_tick][channel] = []
-                mappings[start_tick][channel].append(new_note)
-            if event_type == NoteOnEvent:
-                if event.channel not in running_events:
-                    running_events[event.pitch] = {}
-                running_events[event.pitch][event.channel] = (cumulative_tick, event)
-    # sort by first key, the starting tick
-    mappings = OrderedDict(sorted(mappings.items(), key=lambda key: key[0]))
-    return mappings
-
-
-def print_mappings(mappings):
-    for tick in mappings:
-        for channel in mappings[tick]:
-            if len(mappings[tick][channel]) > 1:
-                print "CHORD:"
-            else:
-                print "NOTE:"
-            for note in mappings[tick][channel]:
-                print note
-
-
-class SoundEvent(object):
-    def __init__(self, notes):
-        self.notes = notes
-
-    def __hash__(self):
-        return hash(self.notes)
+def has_notes(channel_notes):
+    return channel_notes and len(channel_notes) > 0
 
 
 class NGram(object):
     def __init__(self, mappings, n):
         self.mappings = mappings
         self.n = n
-        self.distribution = {}
+        self.tuple_distribution = {}
+        self.event_distribution = {}
         self.tuples = {}
         self.build()
 
     def build(self):
-        tuples_being_built = [()]
+        tuples_being_built = []
 
         for tick in self.mappings:
-
-            notes_in_first_channel = self.mappings[tick][0]
+            first_channel = self.mappings[tick][0]
             # check to see if there are any notes
-            if notes_in_first_channel and len(notes_in_first_channel) > 0:
+            if has_notes(first_channel):
+                sound_event = SoundEvent(self.mappings[tick][0])
                 # create a new tuple
                 tuples_being_built.append(())
                 # append the notes to our list of tuples
-                for ntuple in tuples_being_built:
-                    ntuple += (SoundEvent(self.mappings[tick][0]))
+                for i in range(len(tuples_being_built)):
+                    tuples_being_built[i] += (sound_event,)
+                self.update_event_distribution(sound_event)
             else:
                 continue
-
             # if the first tuple has n elements, update its count, then remove it
-            if len(tuples_being_built[0]) == self.n:
-                tuples_id = hash(tuples_being_built[0])
-                self.tuples[tuples_id] = tuples_being_built[0]
-                # if the tuple is new, then set its count to 1, otherwise update it
-                if self.distribution[tuples_id]:
-                    self.distribution[tuples_id] += 1
-                else:
-                    self.distribution[tuples_id] = 1
-                del tuples_being_built[0]
+            first_tuple = tuples_being_built[0]
+            if len(first_tuple) >= self.n:
+                self.update_tuples(tuples_being_built)
+
+    # for logging purposes
+    def update_event_distribution(self, sound_event):
+        if sound_event not in self.event_distribution:
+            self.event_distribution[sound_event] = 1
+        else:
+            self.event_distribution[sound_event] += 1
+
+    def update_tuples(self, tuples_being_built):
+        first_tuple = tuples_being_built[0]
+        tuples_id = hash(first_tuple)
+        self.tuples[tuples_id] = first_tuple
+        # if the tuple is new, then set its count to 1, otherwise update it
+        if tuples_id not in self.tuple_distribution:
+            self.tuple_distribution[tuples_id] = 1
+        else:
+            self.tuple_distribution[tuples_id] += 1
+        # del tuples_being_built[0]
+        tuples_being_built.pop(0)
+
+    def __str__(self):
+        string = "SoundEventDistribution (unique events=%d)" % len(self.event_distribution) + "\n"
+        string += "SoundEvent: Count \n"
+        for key in self.event_distribution:
+            string += '{}: {}\n'.format(key, self.event_distribution[key])
+        string += ("TupleDistribution (unique tuples=%d)" % len(self.tuple_distribution)) + "\n"
+        string += "Tuple: Count \n"
+        string += self.to_string(self.tuple_distribution)
+        return string
+
+    def to_string(self, distribution):
+        string = ""
+        for key in distribution:
+            for t in self.tuples[key]:
+                string += '{}: {}\n'.format(t, distribution[key])
+        return string
 
 
 class MusicGenerator(object):
-    def __init__(self, ngram, size):
+    def __init__(self, selection_policy, ngram, song_duration):
+        self.selection_policy = selection_policy
         self.ngram = ngram
         self.tuples = ngram.tuples
-        self.distribution = ngram.distribution
-        self.size = size
+        self.distribution = ngram.tuple_distribution
+        # measured in number of notes
+        self.song_duration = song_duration
+        self.sequence = []
 
     def generate(self):
-        sequence = []
-        distribution = self.distribution
         # first tuple
-        tuple = distribution[distribution.keys()[0]]
-        for i in range(0, self.size, 1):
-            for sound_event in tuple:
-                sequence.append(sound_event)
-            tuple = self.next_tuple(tuple[-1])
-        return sequence
+        event_tuple = self.tuples[self.tuples.keys()[0]]
+        for i in range(0, self.song_duration, 1):
+            for sound_event in event_tuple:
+                self.sequence.append(sound_event)
+            event_tuple = self.next_tuple(event_tuple[-1])
+            # we are only concerned about elements after the first one
+            event_tuple = event_tuple[1:]
 
     # find the tuple with the maximum count that starts with last_sound_event
     def next_tuple(self, last_sound_event):
-        tuples = self.tuples
-        distribution = self.distribution
         max_count = 0
         next_tuple = None
-        for (tuple_key, count) in distribution:
-            if tuples[tuple_key][0] == last_sound_event and count > max_count:
+        for key in self.distribution:
+            count = self.distribution[key]
+            t = self.tuples[key]
+            if self.tuples[key][0] == last_sound_event and (count > max_count):
                 max_count = count
-                next_tuple = tuples[tuple_key][0]
+                next_tuple = t
+
+
+        # in NLP, once this condition is reached, the program ends, but it works differently with music
+        if next_tuple is None:
+            if self.selection_policy is SelectionPolicy.RANDOM:
+                i = randint(0, len(self.tuples))
+                print "random int {}".format(i)
+                next_tuple = self.tuples[self.tuples.keys()[i]]
+            if self.selection_policy is SelectionPolicy.NEXT:
+                pass  # no implementation
+            if self.selection_policy is SelectionPolicy.BEFORE_HIGHEST_RANGE:
+                pass  # no implementation
+            if self.selection_policy is SelectionPolicy.AFTER_HIGHEST_RANGE:
+                pass  # no implementation
+
         return next_tuple
 
+    def to_midi_pattern(self):
+        pattern = midi.Pattern()
+        track = midi.Track()
+        for sound_event in self.sequence:
+            if is_chord(sound_event):
+                for note in sound_event.notes:
+                    on_event = midi.NoteOnEvent(channel=note.channel, tick=0, pitch=note.pitch,
+                                                velocity=note.velocity)
+                    track.append(on_event)
 
-mappings = get_note_mappings("mary.mid")
-# print_mappings(mappings)
+    def print_sequence(self):
+        print "SEQUENCE:"
+        for item in self.sequence:
+            print item
 
-bigram = NGram(mappings, 2)
-generator = MusicGenerator(bigram, 10)
-sequence = generator.generate()
-for event in sequence:
-    print event
 
+class SelectionPolicy:
+    def __init__(self):
+        pass
+
+    RANDOM = 0
+    NEXT = 1
+    BEFORE_HIGHEST_RANGE = 2
+    AFTER_HIGHEST_RANGE = 3
+
+
+# define properties
+midi_file = "mary.mid"
+number_of_notes = 2
+selection_policy = SelectionPolicy.RANDOM
+
+# load file
+midi_io = MidiIO(midi_file)
+midi_io.print_events()
+
+# get the note mappings
+mappings = midi_io.mappings
+midi_io.print_mappings()
+
+# construct the bigram
+bigram = NGram(mappings, 3)
+print bigram
+
+# construct the generator and generate a sequence of sound events
+generator = MusicGenerator(selection_policy, bigram, number_of_notes)
+generator.generate()
+generator.print_sequence()
+
+sequence = generator.sequence
